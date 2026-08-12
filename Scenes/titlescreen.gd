70b74a1d915ab330cfbeace8c9243da894a13c00
michelleebe <michelleebesunun@gmail.com>
2026-08-12 15:54:24 -0400

    Player
    I created my player, and i started on my first minigame

--- Scenes/titlescreen.gd
+++ Scenes/titlescreen.gd
@@ -14,4 +14,4 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://levelscene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/levelscene.tscn")
